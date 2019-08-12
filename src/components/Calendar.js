4714c185aa4249d913ca093029f9daad5050c169
Steve Forse <steve.forse@gmail.com>
2019-08-12 06:01:48 +0000

    Add fullcalendar packages and rrule as dependency

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import bootstrapPlugin from '@fullcalendar/bootstrap'
+import rrulePlugin from '@fullcalendar/rrule'
+import listPlugin from '@fullcalendar/list'
+
+import '@fullcalendar/core/main.css'
+import '@fullcalendar/daygrid/main.css'
+import '@fullcalendar/bootstrap/main.css'
+import '@fullcalendar/list/main.css'
+
+import { Card, Row, Col } from 'react-bootstrap'
+
 class  Calendar extends Component {
   render() {
     return (
-      <h1>Calendar goes here</h1>
+      <Card>
+        <Card.Header>
+          <div className="h4">Calendar</div>
+        </Card.Header>
+        <Card.Body className="p-0">
+          <Row>
+            <Col md="12" lg="8" className="mb-4">
+          <FullCalendar
+            defaultView="dayGridMonth"
+            plugins={[ dayGridPlugin, bootstrapPlugin, rrulePlugin ]}
+            themeSystem='bootstrap'
+            fixedWeekCount={false}
+            showNonCurrentDates={false}
+          />
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
     )
   }
 }
